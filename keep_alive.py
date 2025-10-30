c4da31bcd647318e9cd02cac26ecb5c6693700d3
repazasepa-dotcom <repazasepa@gmail.com>
2025-10-31 00:24:06 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,14 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "PumpFun Bot Alive ✅"
 
 def run():
     app.run(host="0.0.0.0", port=5000)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    Thread(target=run).start()
