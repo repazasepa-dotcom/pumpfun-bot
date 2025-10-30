eeb8cc127c3b3706463ca7b1506a5c0e264aed9c
repazasepa-dotcom <repazasepa@gmail.com>
2025-10-30 22:40:11 +0800

    Update wallet_watch.py

--- wallet_watch.py
+++ wallet_watch.py
@@ -1,1 +1,15 @@
+# wallet_watch.py
+# Simple poller for a list of wallets: checks recent transfers (via Birdeye or Solscan)
+import time
+from birdeye_api import token_info
+import os, requests
 
+WATCHED = os.getenv("WATCH_WALLETS", "").split(",")  # comma separated addresses
+
+def check_wallets():
+    results = []
+    for w in WATCHED:
+        if not w: continue
+        # call an API (Birdeye or Solscan) to get recent transfers
+        # For brevity: placeholder to be implemented with your preferred provider
+    return results
