23b71fd5dc94ffa0677fd1cca422d337142aa03a
repazasepa-dotcom <repazasepa@gmail.com>
2025-10-31 18:34:32 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -2,11 +2,11 @@ from flask import Flask
 import os
 
 PORT = int(os.getenv("PORT", "10000"))
-app_flask = Flask("KeepAlive")
+app = Flask("KeepAlive")
 
-@app_flask.route("/")
+@app.route("/")
 def home():
     return "Bot is alive!"
 
 def run_flask():
-    app_flask.run(host="0.0.0.0", port=PORT)
+    app.run(host="0.0.0.0", port=PORT)
